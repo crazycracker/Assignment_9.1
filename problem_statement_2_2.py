c2e6b64186b860f8320a772588a62fcc30dee919
vinay <vinayraj33@live.com>
2018-06-13 19:14:31 +0530

    second code done

--- problem_statement_2_2.py
+++ problem_statement_2_2.py
@@ -6,5 +6,5 @@ import numpy as np
     step 2: creating a series with date of random numbers using numpy and index as the dateTimeIndices
 '''
 dateTimeIndices = pd.date_range('1/1/2015','31/12/2015')
-series_of_random_numbers = pd.Series(np.random.randint(0,100,len(dateTimeIndices)),index=dateTimeIndices)
+series_of_random_numbers = pd.Series(np.random.randint(0, 100 ,len(dateTimeIndices)),index=dateTimeIndices)
 print(series_of_random_numbers)
