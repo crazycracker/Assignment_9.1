0f31a602215a61ca7fdd89e07303d39c5887f82d
vinay <vinayraj33@live.com>
2018-06-13 19:12:42 +0530

    third code done

--- problem_statement_2_2.py
+++ problem_statement_2_2.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 
-series_of_dates = pd.bdate_range('2015-01-01','2015-12-31')
-df = pd.DataFrame(np.random.randint(0,100,size=(1,len(series_of_dates))), columns=series_of_dates)
-print(df)
+'''
+    step 1: creating a dateTimeIndices using date_range
+    step 2: creating a series with date of random numbers using numpy and index as the dateTimeIndices
+'''
+dateTimeIndices = pd.date_range('1/1/2015','31/12/2015')
+series_of_random_numbers = pd.Series(np.random.randint(0,100,len(dateTimeIndices)),index=dateTimeIndices)
+print(series_of_random_numbers)
